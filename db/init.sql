507243d4a5645f37db828bcab0d36ffc7023f68a
root <root@servidorUbuntu>
2023-05-08 21:38:16 +0000

    Creado el repo: funciona haproxy, db a medias y falta corregir algunas cosas de los php

---
+++ db/init.sql
@@ -0,0 +1,68 @@
+CREATE DATABASE enikio;
+use enikio;
+
+CREATE TABLE usuarios (
+        cc varchar(10),
+        nombre varchar(50),
+        email varchar(50),
+        password varchar(50),
+        celular varchar(10),
+        rol varchar(10),
+        primary key (cc));
+
+
+CREATE TABLE aptos(
+        link varchar(150),
+        id_apto int,
+        precio int,
+        cant_h int,
+        coord point,
+        hab_disponibles int,
+        id_arrendador int,
+        primary key (id_apto));
+
+CREATE TABLE postulaciones(
+        id_apto int,
+        cc_postulado varchar(10),
+        fecha date,
+        ocupacion varchar(50),
+        interes varchar(20),
+        estado varchar(20));
+
+CREATE TABLE universidades(
+        nombre varchar(50),
+        coord point);
+
+LOAD DATA INFILE '/var/lib/mysql-files/usuarios.csv'
+INTO TABLE usuarios
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/aptos.csv'
+INTO TABLE aptos
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(link, id_apto, precio, cant_h, @coord, hab_disponibles, id_arrendador)
+SET coord = ST_PointFromText(@coord);
+
+LOAD DATA INFILE '/var/lib/mysql-files/postulaciones.csv'
+INTO TABLE postulaciones
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(id_apto, cc_postulado, @fecha, ocupacion, interes, estado)
+SET fecha = STR_TO_DATE(@fecha, '%Y-%m-%d');
+
+LOAD DATA INFILE '/var/lib/mysql-files/universidades.csv'
+INTO TABLE universidades
+FIELDS TERMINATED BY ';'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(nombre, @coord)
+SET coord = ST_PointFromText(@coord);
